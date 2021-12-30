8f816f20113a0d9385edc50136bebbe2d45bba3c
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-12-31 03:25:03 +0330

    first try with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Movies from './components/Movies'
-import MovieDetail from './components/MovieDetail'
-import NotFound from './components/NotFound'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Movies from './components/Movies';
+import MovieDetail from './components/MovieDetail';
+import NotFound from './components/NotFound';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:9000/movies/')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
   // your code here ...
-  return <></>
+  return <></>;
 }
 
-export default App
+export default App;
