f320c11644c554f78929643e3633ae03be29f208
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-12-31 05:15:25 +0330

    MovieCard ready

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 function MovieCard({ movie }) {
   return (
-    <div className="movie-card">
-      <img className="card-img" src={movie.imageUrl} alt="" />
-      <div className="card-body">
-        <h2 className="card-title">{movie.title}</h2>
-        <p className="card-description">{movie.short_description}</p>
+    <>
+      <div className="movie-card">
+        <Link to={`/movies/${String(movie.id)}`}>
+          <img className="card-img" src={movie.imageUrl} alt="" />
+          <div className="card-body">
+            <h2 className="card-title">{movie.title}</h2>
+            <p className="card-description">{movie.short_description}</p>
+          </div>
+        </Link>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
