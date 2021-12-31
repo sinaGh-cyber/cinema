073edb2cbca840e682f8b32cb4e10fe330059dbd
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-12-31 04:55:01 +0330

    App ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect } from 'react';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Movies from './components/Movies';
 import MovieDetail from './components/MovieDetail';
 import NotFound from './components/NotFound';
 
 function App() {
-  useEffect(() => {
-    fetch('http://localhost:9000/movies/')
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  });
-  // your code here ...
-  return <></>;
+
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path={'/'} component={Movies} />
+          <Route path={'/movies/:movie_id'} component={MovieDetail}/>
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
